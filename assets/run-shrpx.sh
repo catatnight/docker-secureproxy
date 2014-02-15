01b5b3063e562b3583002261356fe78294ab7157
Minghou Ye <minghou.ye@gmail.com>
2014-02-15 11:52:13 +0800

    Delete spaces at the end of lines
    shrpx config file DO NOT accept space at line endings

--- assets/run-shrpx.sh
+++ assets/run-shrpx.sh
@@ -5,7 +5,7 @@ frontend=0.0.0.0,$shrpx_port\n\
 backend=127.0.0.1,8000\n\
 private-key-file=/root/certs/server.key\n\
 certificate-file=/root/certs/server.crt\n\
-spdy-proxy=yes \n\
+spdy-proxy=yes\n\
 daemon=no\n\
 workers=1\n" \
 > /etc/shrpx/shrpx.conf
