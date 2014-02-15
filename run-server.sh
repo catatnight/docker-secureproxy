9e99d628abdba8b968ad0bbe5d68fb5f45066b4b
Minghou Ye <minghou.ye@gmail.com>
2014-02-15 11:43:28 +0800

    update tip's wording

--- run-server.sh
+++ run-server.sh
@@ -2,5 +2,5 @@
 
 docker run -p 8222:8222 -name spdyproxy -d catatnight/spdyproxy
 
-#if shrpx is connected with a local container, please use
+#if squid3 is linked to a local docker container running freeradius, please use
 #docker run -p 8222:8222 -name spdyproxy -d -link <container>:<alias> catatnight/spdyproxy
