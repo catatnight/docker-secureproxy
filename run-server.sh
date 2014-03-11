848d3d9aa963302cf2ca714c868398621ac0c616
Minghou Ye <minghou.ye@gmail.com>
2014-03-11 11:49:06 +0800

    "docker run -name" is depreciated

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -p 8222:8222 -name spdyproxy -d catatnight/spdyproxy
+docker run -p 8222:8222 --name spdyproxy -d catatnight/spdyproxy
 
 #if squid3 is linked to a local docker container running freeradius, please use
 #docker run -p 8222:8222 -name spdyproxy -d -link <container>:<alias> catatnight/spdyproxy
