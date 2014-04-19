5dc39cf5db9ddd59c2ac784bab41fdbe378fd3d8
Minghou Ye <minghou.ye@gmail.com>
2014-04-19 09:55:05 +0800

    small fix

--- run-server.sh
+++ run-server.sh
@@ -3,4 +3,4 @@
 docker run -p 8222:8222 --name spdyproxy -d catatnight/spdyproxy
 
 #if squid3 is linked to a local docker container running freeradius, please use
-#docker run -p 8222:8222 -name spdyproxy -d --link <container>:<alias> catatnight/spdyproxy
+#docker run -p 8222:8222 --name spdyproxy -d --link <container>:<alias> catatnight/spdyproxy
