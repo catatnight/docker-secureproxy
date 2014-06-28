8aa351cbba740938a1552d8e90cecc50817df06a
Minghou Ye <minghou.ye@gmail.com>
2014-06-28 15:46:42 +0800

    simplify build.sh

--- assets/build-deb.sh
+++ assets/build-deb.sh
@@ -5,19 +5,19 @@ apt-get update && apt-get --no-install-recommends -y install wget
 shrpx_v=$(wget --no-check-certificate -O - http://github.com/tatsuhiro-t/spdylay/releases | grep -o '[0-9]\.[0-9]\.[0-9]' | head -1)
 if [[ ! -f /tmp/spdylay.deb || "$(dpkg-deb --field /tmp/spdylay.deb version)" != "$shrpx_v-1" ]]; then
 	apt-get -y install build-essential checkinstall autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libevent-dev 
-	cd /tmp && wget --no-check-certificate https://github.com/tatsuhiro-t/spdylay/releases/download/v$shrpx_v/spdylay-$shrpx_v.tar.gz
+	cd / && wget --no-check-certificate https://github.com/tatsuhiro-t/spdylay/releases/download/v$shrpx_v/spdylay-$shrpx_v.tar.gz
 	tar -zxvf spdylay-$shrpx_v.tar.gz && cd spdylay-$shrpx_v/
 	autoreconf -i && automake && autoconf && ./configure && make
 	checkinstall -y --requires="libevent-openssl-2.0-5, libevent-2.0-5" --install=no --maintainer=tatsuhiro-t
-	cp /tmp/spdylay-$shrpx_v/spdylay_$shrpx_v-1_amd64.deb /tmp/spdylay.deb
+	cp /spdylay-$shrpx_v/spdylay_$shrpx_v-1_amd64.deb /tmp/spdylay.deb
 fi
  
 nghttp2_v=$(wget --no-check-certificate -O - http://github.com/tatsuhiro-t/nghttp2/releases | grep -o '[0-9]\.[0-9]\.[0-9]' | head -1)
 if [[ ! -f /tmp/nghttp2.deb || "$(dpkg-deb --field /tmp/nghttp2.deb version)" != "$nghttp2_v-1" ]]; then
 	apt-get -y install build-essential checkinstall autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libevent-dev libjansson-dev libjemalloc-dev cython python3.4-dev
-	cd /tmp && wget --no-check-certificate https://github.com/tatsuhiro-t/nghttp2/releases/download/v$nghttp2_v/nghttp2-$nghttp2_v.tar.gz
+	cd / && wget --no-check-certificate https://github.com/tatsuhiro-t/nghttp2/releases/download/v$nghttp2_v/nghttp2-$nghttp2_v.tar.gz
 	tar -zxvf nghttp2-$nghttp2_v.tar.gz && cd nghttp2-$nghttp2_v
 	autoreconf -i && automake && autoconf && ./configure && make
 	checkinstall -y --requires="libevent-openssl-2.0-5, libevent-2.0-5, libjemalloc-dev" --install=no --maintainer=tatsuhiro-t
-	cp /tmp/nghttp2-$nghttp2/nghttp2_$nghttp2_v-1_amd64.deb /tmp/nghttp2.deb
+	cp /nghttp2-$nghttp2_v/nghttp2_$nghttp2_v-1_amd64.deb /tmp/nghttp2.deb
 fi
--- build.sh
+++ build.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-cd assets/
-
-bash -c "cat > Dockerfile" <<-EOF
-From ubuntu:trusty
-ADD build-deb.sh /tmp/build-deb.sh
-RUN /tmp/build-deb.sh
-EOF
-
-if [ -f spdylay.deb ]; then
-	sed -i "/From.*/a ADD spdylay.deb \/tmp\/spdylay.deb" Dockerfile
-fi
-if [ -f nghttp2.deb ]; then
-  sed -i "/From.*/a ADD nghttp2.deb \/tmp\/nghttp2.deb" Dockerfile
-fi
-
 img='secureproxy'
-docker build --no-cache --rm -t $img-temp .
-docker run -itd --name $img-temp $img-temp tail -f /var/log/dpkg.log
-docker cp $img-temp:/tmp/spdylay.deb . 
-docker cp $img-temp:/tmp/nghttp2.deb . 
-docker rm -f $img-temp && docker rmi $img-temp && rm Dockerfile
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/assets/"
 
-cd ..
+docker run -v $DIR:/tmp --rm --name $img-temp ubuntu sh -c "/tmp/build-deb.sh && echo success"
 
 docker build --rm -t catatnight/$img .
