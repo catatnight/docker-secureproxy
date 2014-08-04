e2ddedf093d33f430d536aa0c6ffad0c5db93b57
Minghou Ye <minghou.ye@gmail.com>
2014-08-04 12:50:26 +0800

    docker build --rm=true (default)

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/assets/"
 
 docker run -it -v $DIR:/tmp --rm ubuntu:trusty sh -c "/tmp/build-deb.sh && echo success"
 
-docker build --rm -t catatnight/secureproxy .
+docker build -t catatnight/secureproxy .
