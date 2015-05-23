f5be22c19925bcfb701b2dd63609494dd15b7b86
Minghou Ye <minghou.ye@gmail.com>
2015-05-23 18:57:36 +0800

    build nghttp2 with spdylay

--- assets/build-deb.sh
+++ assets/build-deb.sh
@@ -19,11 +19,15 @@ EOF
 nghttp2_v=`cat /nghttp2`
 if [[ ! -f /tmp/nghttp2.deb || "$(dpkg-deb --field /tmp/nghttp2.deb version)" != "$nghttp2_v-1" ]]; then
 	apt-get update
-	apt-get -y install wget build-essential checkinstall autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libevent-dev libjansson-dev libjemalloc-dev cython python3.4-dev
+	apt-get -y install wget build-essential checkinstall autoconf automake autotools-dev libtool pkg-config zlib1g-dev libcunit1-dev libssl-dev libxml2-dev libev-dev libevent-dev libjansson-dev libjemalloc-dev cython python3.4-dev
+	spdylay_v=`cat /spdylay`
+	cd / && wget --no-check-certificate https://github.com/tatsuhiro-t/spdylay/releases/download/v$spdylay_v/spdylay-$spdylay_v.tar.gz
+	tar -zxvf spdylay-$spdylay_v.tar.gz && cd spdylay-$spdylay_v/
+	autoreconf -i && automake && autoconf && ./configure && make install
 	cd / && wget --no-check-certificate https://github.com/tatsuhiro-t/nghttp2/releases/download/v$nghttp2_v/nghttp2-$nghttp2_v.tar.gz
 	tar -zxvf nghttp2-$nghttp2_v.tar.gz && cd nghttp2-$nghttp2_v
 	autoreconf -i && automake && autoconf && ./configure && make
-	checkinstall -y --requires="libevent-openssl-2.0-5, libevent-2.0-5, libjemalloc-dev" --install=no --maintainer=tatsuhiro-t
+	checkinstall -y --requires="libevent-openssl-2.0-5, libevent-2.0-5, libjemalloc-dev, libev-dev" --install=no --maintainer=tatsuhiro-t
 	cp /nghttp2-$nghttp2_v/nghttp2_$nghttp2_v-1_amd64.deb /tmp/nghttp2.deb
 fi
 
