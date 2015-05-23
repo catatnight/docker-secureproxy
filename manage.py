47606f7469ff6949c54ae4a2d7941cb0eac67675
Minghou Ye <minghou.ye@gmail.com>
2015-05-23 18:58:28 +0800

    add debug mode to manage.py

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ class bcolors:
 app_name = 'secureproxy'
 
 parser = argparse.ArgumentParser(description='Manage %s container' % app_name)
-parser.add_argument("execute", choices=['create','start','stop','restart','delete'], help='manage %s server' % app_name)
+parser.add_argument("execute", choices=['create','debug','start','stop','restart','delete'], help='manage %s server' % app_name)
 parser.add_argument("-p","--proxy_port", default="", help="proxy port")
 parser.add_argument("--radius_server", default="", help="radius server address")
 parser.add_argument("--radius_secret", default="", help="radius server secret")
@@ -30,11 +30,19 @@ cmd_dict = {\
 		"-e radius_server={2} -e radius_radpass={3} -e ncsa_users={4} "\
 		"-e time_zone=Asia/Shanghai -v {5}/certs:/opt/certs -d catatnight/{0}"\
 		.format(app_name, args.proxy_port, args.radius_server, args.radius_secret, args.ncsa_users, os.path.dirname(os.path.realpath(__file__))), \
+	"debug" : \
+		"docker run --net=host --name {0} -e proxy_port={1} "\
+		"-e radius_server={2} -e radius_radpass={3} -e ncsa_users={4} "\
+		"-e time_zone=Asia/Shanghai -v {5}/certs:/opt/certs -it catatnight/{0} /bin/bash"\
+		.format(app_name, args.proxy_port, args.radius_server, args.radius_secret, args.ncsa_users, os.path.dirname(os.path.realpath(__file__))), \
 	"start"  : "docker start   %s" % app_name, \
 	"stop"   : "docker stop    %s" % app_name, \
 	"restart": "docker restart %s" % app_name, \
 	"delete" : "docker rm -f   %s" % app_name}
 cmd_key = args.execute
+if cmd_key  == 'debug':
+	process = subprocess.call(shlex.split(cmd_dict[cmd_key]))
+	exit(0)
 if cmd_key == "create" and args.proxy_port == "":
 	sys.exit(bcolors.WARNING + "Error: Proxy port must be set. " + bcolors.ENDC)
 
